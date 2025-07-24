2215d16ec3b88a578744d2bdb522f8c32435172b
EJaishnavi <jaishnavireddy9010.chv@outlook.com>
2025-07-24 21:27:29 +0530

    Create resource.tf

---
+++ resource.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "server1" {
+ tag{
+ Name = "ec2-server1"
+}
+instance_type = "micro"
+ami = "ami-0e0dab2e7d3136219"
+key_name = "Mahadev"
+availability_zone = "us-east-1a"
+vpc_security_group_ids = "sg-0d31b7d6f4c99f15e"
+root_block_device{
+volume_size = 25
+}
+count = 1
+}
