4381119bbcac6226bd77788d8e9b0d06c3de28ea
EJaishnavi <jaishnavireddy9010.chv@outlook.com>
2025-07-30 22:21:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ resource "aws_instance" "server1" {
  tag{
  Name = "ec2-server1"
 }
-instance_type = "micro"
+instance_type = "t3.micro"
 ami = "ami-0e0dab2e7d3136219"
 key_name = "Mahadev"
 availability_zone = "us-east-1a"
-vpc_security_group_ids = "sg-0d31b7d6f4c99f15e"
+vpc_security_group_ids = ["sg-0d31b7d6f4c99f15e"] 
 root_block_device{
 volume_size = 20
 }
